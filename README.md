8e66255584e53497fbeb2e94660373e015987a06
Ruslan Gainutdinov <contact@ruslan.org>
2023-04-15 15:21:18 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# tw-webapp
+
+Runs this [api](https://github.com/trevorhobenshield/twitter-api-client) in a webapp.
---
+++ app.py
@@ -0,0 +1,33 @@
+import re
+from flask import Flask, request, jsonify
+from twitter.search import search
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app, resources={r"/*": {"origins": "*"}})
+
+@app.route('/')
+def home():
+    """Home page."""
+    return 'Wow, it works!'
+
+@app.route('/search')
+def twsearch():
+    """Search Twitter for a query."""
+    q = request.args.get('q')
+    if not q:
+        return error_response('Please provide a query.')
+
+    try:
+        result = search(q)
+        return jsonify(result)
+    except Exception as err:
+        return error_response(f'Error searching: {err}')
+
+
+def error_response(message):
+    response = jsonify([{
+        'message': message,
+    }])
+    
+    return response, 400
